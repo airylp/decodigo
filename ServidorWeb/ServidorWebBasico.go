4fab9ae2d5c88689f140da79e63e2eed2f6f57d1
Ant <airylp@gmail.com>
2022-06-20 19:39:48 -0500

    Servidor Web Básico

---
+++ ServidorWeb/ServidorWebBasico.go
@@ -0,0 +1,18 @@
+package main
+
+//decodigo.com
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Saludos, bienvenido a %s!", r.URL.Path[1:])
+}
+
+func main() {
+	http.HandleFunc("/", handler)
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
